aa8c20a910bfb06ad8764e0dc697efbfd3173dff
keithhedger <keithhedger@87548d4b-bfdf-4437-b577-269a87841b76>
2014-09-29 10:36:42 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEditPlugins@119 87548d4b-bfdf-4437-b577-269a87841b76

---
+++ ClipboardPlugin/clipboardplugin.cpp
@@ -0,0 +1,167 @@
+/*
+  clipboardplugin.cpp
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <gtk/gtk.h>
+#include <gmodule.h>
+#include <libgen.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <kkedit-plugins.h>
+
+#define MYEMAIL "kdhedger68713@gmail.com"
+#define MYWEBSITE "http://keithhedger.hostingsiteforfree.com/index.html"
+#define VERSION "0.0.1"
+
+char*		prefsPath;
+GtkWidget*	menuPlug;
+
+int	(*module_plug_function)(gpointer globaldata);
+
+extern "C" const gchar* g_module_check_init(GModule *module)
+{
+	return(NULL);
+}
+
+extern "C" const gchar* g_module_unload(GModule *module)
+{
+	return(NULL);
+}
+
+void runCommandAndOut(char* command,plugData* plugdata)
+{
+	FILE*		fp=NULL;
+	char		line[1024];
+	GtkTextIter	iter;
+
+	fp=popen(command,"r");
+	if(fp!=NULL)
+		{
+			while(fgets(line,1024,fp))
+				{
+					gtk_text_buffer_insert_at_cursor(plugdata->toolOutBuffer,line,strlen(line));
+					while(gtk_events_pending())
+						gtk_main_iteration();
+					gtk_text_buffer_get_end_iter(plugdata->toolOutBuffer,&iter);
+					gtk_text_view_scroll_to_iter((GtkTextView*)plugdata->toolOutWindow,&iter,0,true,0,0);
+				}
+			pclose(fp);
+		}
+}
+
+void theCallBack(GtkWidget* widget,gpointer data)
+{
+	plugData*	plugdata=(plugData*)data;
+
+	showToolOutput(true);
+	runCommandAndOut((char*)"ls /",plugdata);
+}
+
+extern "C" int addToGui(gpointer data)
+{
+	GtkWidget*	menuitem;
+	GtkWidget*	menu;
+
+	plugData*	plugdata=(plugData*)data;
+
+	menuPlug=gtk_menu_item_new_with_label("_NewKKeditPlug Menu");
+	gtk_menu_item_set_use_underline((GtkMenuItem*)menuPlug,true);
+	menu=gtk_menu_new();
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(menuPlug),menu);
+
+	menuitem=gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW,NULL);
+	gtk_signal_connect(GTK_OBJECT(menuitem),"activate",G_CALLBACK(theCallBack),plugdata);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu),menuitem);
+
+	gtk_menu_shell_append(GTK_MENU_SHELL(plugdata->mlist.menuBar),menuPlug);					
+
+	return(0);
+}
+
+extern "C" int plugPrefs(gpointer data)
+{
+	GtkWidget*	dialog;
+	GtkWidget*	dialogbox;
+	GtkWidget*	prefs;
+	GtkWidget*	vbox;
+	int			response;
+	char*		command;
+	plugData*	plugdata=(plugData*)data;
+
+	vbox=gtk_vbox_new(false,0);
+
+	dialog=gtk_dialog_new_with_buttons("ClipboardPlugin",NULL,GTK_DIALOG_MODAL,GTK_STOCK_APPLY,GTK_RESPONSE_APPLY,GTK_STOCK_CANCEL,GTK_RESPONSE_CANCEL,NULL);
+	gtk_window_set_default_size((GtkWindow*)dialog,300,120);
+	dialogbox=gtk_dialog_get_content_area((GtkDialog*)dialog);
+	gtk_container_add(GTK_CONTAINER(dialogbox),vbox);
+
+	prefs=gtk_entry_new();
+
+	gtk_entry_set_text((GtkEntry*)prefs,prefsPath);
+	gtk_box_pack_start((GtkBox*)vbox,gtk_label_new("Prefs Path"),true,true,4);
+	gtk_box_pack_start((GtkBox*)vbox,prefs,true,true,4);
+
+	gtk_widget_show_all(dialog);
+	response=gtk_dialog_run(GTK_DIALOG(dialog));
+	if(response==GTK_RESPONSE_APPLY);
+		{
+			asprintf(&command,"echo %s>%s/clipboardplugin.rc",gtk_entry_get_text((GtkEntry*)prefs),plugdata->lPlugFolder);
+			system(command);
+			free(command);
+		}
+	gtk_widget_destroy((GtkWidget*)dialog);
+	return(0);
+}
+
+extern "C" int doAbout(gpointer data)
+{
+	plugData*		plugdata=(plugData*)data;
+	char*			licencepath;
+	const char		copyright[] ="Copyright \xc2\xa9 2014 K.D.Hedger";
+	const char*		aboutboxstring="ClipboardPlugin";
+	char*			licence;
+	GtkAboutDialog*	about;
+
+	const char*	authors[]= {"K.D.Hedger <"MYEMAIL">\n",MYWEBSITE,"\nMore by the same author\n","Xfce-Theme-Manager\nhttp://xfce-look.org/content/show.php?content=149647\n","Xfce4-Composite-Editor\nhttp://gtk-apps.org/content/show.php/Xfce4-Composite-Editor?content=149523\n","Manpage Editor\nhttp://gtk-apps.org/content/show.php?content=160219\n","GtkSu\nhttp://gtk-apps.org/content/show.php?content=158974\n","ASpell GUI\nhttp://gtk-apps.org/content/show.php/?content=161353\n","Clipboard Viewer\nhttp://gtk-apps.org/content/show.php/?content=121667",NULL};
+
+	asprintf(&licencepath,"%s/docs/gpl-3.0.txt",plugdata->dataDir);
+
+	g_file_get_contents(licencepath,&licence,NULL,NULL);
+	about=(GtkAboutDialog*)gtk_about_dialog_new();
+	gtk_about_dialog_set_program_name(about,"ClipboardPlugin");
+	gtk_about_dialog_set_authors(about,authors);
+	gtk_about_dialog_set_comments(about,aboutboxstring);
+	gtk_about_dialog_set_copyright(about,copyright);
+	gtk_about_dialog_set_version(about,VERSION);
+	gtk_about_dialog_set_website(about,MYWEBSITE);
+	gtk_about_dialog_set_logo_icon_name(about,"KKEditPlug");
+	gtk_about_dialog_set_license(about,licence);
+
+	gtk_dialog_run(GTK_DIALOG(about));
+	gtk_widget_destroy((GtkWidget*)about);
+	free(licence);
+	free(licencepath);
+	return(0);
+}
+
+extern "C" int enablePlug(gpointer data)
+{
+	plugData*		plugdata=(plugData*)data;
+
+	if(plugdata->modData->unload==true)
+		{
+			gtk_widget_destroy(menuPlug);
+			gtk_widget_show_all(plugdata->mlist.menuBar);	
+		}
+	else
+		{
+			if(g_module_symbol(plugdata->modData->module,"addToGui",(gpointer*)&module_plug_function))
+				module_plug_function(data);
+			gtk_widget_show_all(plugdata->mlist.menuBar);
+		}
+	return(0);
+}

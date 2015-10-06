5f0a8f537234a442abe90dfd277c72e7d1fe21b6
keithhedger <keithhedger@87548d4b-bfdf-4437-b577-269a87841b76>
2015-10-06 11:25:22 +0000

    git-svn-id: file:///media/LinuxData/Development/SVN/KKEditPlugins@178 87548d4b-bfdf-4437-b577-269a87841b76

--- SessionManager/sessionmanager.cpp
+++ SessionManager/sessionmanager.cpp
@@ -1,6 +1,23 @@
 /*
-  sessionmanager.cpp
-*/
+ *
+ * ©K. D. Hedger. Tue  6 Oct 12:24:38 BST 2015 kdhedger68713@gmail.com
+
+ * This file (sessionmanager.cpp) is part of KKEditPlugins.
+
+ * KKEditPlugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+
+ * KKEditPlugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with KKEditPlugins.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #include <stdio.h>
 #include <stdlib.h>
